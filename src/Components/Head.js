8db74ac9566ea2c9ebcabc750f7e37272506fce1
sar653 <sarva653@gmail.com>
2024-06-28 20:17:08 +0530

    first commit

--- src/Components/Head.js
+++ src/Components/Head.js
@@ -142,7 +142,7 @@ const Head = () => {
       </div>
 
      <Link to ="/SignUp"> <div className="col-span-1">
-      <img
+        <img
           className="h-8"
           alt="user"
           src="https://t3.ftcdn.net/jpg/05/53/79/60/360_F_553796090_XHrE6R9jwmBJUMo9HKl41hyHJ5gqt9oz.jpg"
